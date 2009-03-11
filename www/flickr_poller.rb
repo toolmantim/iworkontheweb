d9ad4f4e6f27652813a0d8c48db13ca3f2a8ae98
Tim Lucas <t.lucas@toolmantim.com>
2009-03-11 11:07:01 -0700

    Updated the poller to mach (untested)

--- www/flickr_poller.rb
+++ www/flickr_poller.rb
@@ -1,12 +1,13 @@
-require File.join(File.dirname(__FILE__), 'iworkontheweb')
-require File.join(File.dirname(__FILE__), 'parallel')
+__DIR__ = File.dirname(__FILE__)
 
-Iworkontheweb.establish_db_connection
+require "#{__DIR__}/models"
+
+require "#{__DIR__}/parallel"
 
 require 'rexml/document'
 require 'open-uri'
 
-class Iworkontheweb::Flickr
+class FlickrPoller
   API_KEY = "11f5a2f3ae888c99f2da5a8c70411584"
   API_URL_BASE = "http://api.flickr.com/services/rest"
   LIMIT = 500
@@ -21,7 +22,7 @@ class Iworkontheweb::Flickr
         new($1, $2, $3) if string =~ /(.*)\:(.*)\=(.*)/
       end
     end
-    
+  
     def self.from_photo_info(e)
       photo = Photo.new(*%w(id farm secret server).map {|a| e.attributes[a]})
       photo.description = e.get_elements("description").first.text
@@ -31,19 +32,19 @@ class Iworkontheweb::Flickr
       photo.photo_page_url = e.get_elements("//url[@type='photopage']").first.text
       photo      
     end
-    
+  
     def self.iwotw_name_tagged_photos
       flickr_api_result(get_tagged_photos_url).get_elements("//photo").parallel_map(MAX_THREADS) do |e|
         from_photo_info flickr_api_result(get_info_url(e.attributes["id"])).get_elements("//photo").first
       end
     end
-    
+  
     def sizes
       @sizes ||= self.class.flickr_api_result(get_sizes_url).get_elements("//size").map do |e|
         Size.new(*%w(label width height source url).map {|a| e.attributes[a]})
       end
     end
-    
+  
     def to_person_attributes
       {
         "name" => name_tag.value,
@@ -65,7 +66,7 @@ class Iworkontheweb::Flickr
     def to_s
       "#{id}: #{name_tag.value}"
     end
-        
+      
     protected
       def get_sizes_url
         "#{API_URL_BASE}/?method=flickr.photos.getSizes&api_key=#{API_KEY}&photo_id=#{self.id}"
@@ -92,9 +93,9 @@ class Iworkontheweb::Flickr
   end
 
   def self.update!
-    people = Iworkontheweb::Models::Person.find(:all, :select => 'id, name, flickr_photo_id, deleted_at', :conditions => 'deleted_at IS NULL')
+    people = Person.find(:all, :select => 'id, name, flickr_photo_id, deleted_at', :conditions => 'deleted_at IS NULL')
     IWOTW_LOGGER.info "#{people.length} existing people in the DB"
-    
+  
     flickr_photos = Photo.iwotw_name_tagged_photos
     IWOTW_LOGGER.info "#{flickr_photos.length} tagged Flickr Photos"
 
@@ -102,11 +103,11 @@ class Iworkontheweb::Flickr
       IWOTW_LOGGER.error "Flickr returned no photos!"
       return
     end
-    
+  
     deleted_people = people.find_all {|person| !flickr_photos.any? {|photo| photo.id == person.flickr_photo_id }}
     IWOTW_LOGGER.info "#{deleted_people.length} people no longer on Flickr (#{deleted_people.map(&:name).join(', ')})"
     delete_people_no_longer_with_photos(deleted_people) unless deleted_people.empty?
-    
+  
     existing_photos = flickr_photos.find_all {|photo| people.any? {|person| person.flickr_photo_id == photo.id }}
     IWOTW_LOGGER.info "#{existing_photos.length} photos on Flickr already in DB (#{existing_photos.map(&:to_s).join(', ')})"
     update_people_from_flickr_photos(existing_photos) unless existing_photos.empty?
@@ -115,24 +116,24 @@ class Iworkontheweb::Flickr
     IWOTW_LOGGER.info "#{new_photos.length} new photos on Flickr (#{new_photos.map(&:to_s).join(', ')})"
     add_people_from_flickr_photos(new_photos) unless new_photos.empty?
   end
-  
+
   def self.update_people_from_flickr_photos(flickr_photos)
     IWOTW_LOGGER.debug "Updating #{flickr_photos.length} existing people"
     flickr_photos.parallel_map(MAX_THREADS) do |photo|
       [photo, photo.to_person_attributes]
     end.each do |(photo, person_attributes)|
-      Iworkontheweb::Models::Person.find_by_flickr_photo_id(photo.id).update_attributes_if_changed!(person_attributes)
+      Person.find_by_flickr_photo_id(photo.id).update_attributes_if_changed!(person_attributes)
     end
   end
   def self.add_people_from_flickr_photos(new_flickr_photos)
     IWOTW_LOGGER.debug "Adding #{new_flickr_photos.length} people from new flickr photos (#{new_flickr_photos.map(&:id).join(',')})"
     new_flickr_photos.each do |photo|
-      previously_deleted_peep = Iworkontheweb::Models::Person.find_by_flickr_photo_id(photo.id)
+      previously_deleted_peep = Person.find_by_flickr_photo_id(photo.id)
       if previously_deleted_peep
         previously_deleted_peep.update_attributes_if_changed!(photo.to_person_attributes)
         previously_deleted_peep.update_attribute(:deleted_at, nil)
       else
-        Iworkontheweb::Models::Person.new(photo.to_person_attributes).save!
+        Person.new(photo.to_person_attributes).save!
       end
     end
   end
@@ -143,5 +144,5 @@ class Iworkontheweb::Flickr
 end
 
 if $0 == __FILE__
-  Iworkontheweb::Flickr.update!
+  FlickrPoller.update!
 end
