8744c1ccf35c8c3a9b6f1f1c2a3e5e6b077d09dd
Tim Lucas <t.lucas@toolmantim.com>
2009-08-31 00:03:36 +1000

    Fix the path to the pollers

--- www/flickr_poller.sh
+++ www/flickr_poller.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-env IWOTW_ENV=production /usr/bin/ruby /var/www/iworkontheweb/app/flickr_poller.rb > /dev/null 2>&1
+env IWOTW_ENV=production /usr/bin/ruby /var/www/iworkontheweb/app/current/www/flickr_poller.rb > /dev/null 2>&1
