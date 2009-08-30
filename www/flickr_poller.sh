cf72e2ab403fe273657e29702aa83d521eba4faa
Tim Lucas <t.lucas@toolmantim.com>
2009-08-31 00:08:40 +1000

    Ruby ent

--- www/flickr_poller.sh
+++ www/flickr_poller.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-env IWOTW_ENV=production /usr/bin/ruby /var/www/iworkontheweb/app/current/www/flickr_poller.rb > /dev/null 2>&1
+env IWOTW_ENV=production /opt/ruby-enterprise/bin/ruby /var/www/iworkontheweb/app/current/www/flickr_poller.rb > /dev/null 2>&1
